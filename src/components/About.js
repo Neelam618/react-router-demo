9e9e15b5226665025a49c1b8056515d6869d1678
Neelam618 <neelamchavan410@gmail.com>
2021-05-16 17:29:33 +0530

    router example added

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function About() {
+    return (
+        <div>
+            <div>
+                <h1>About Page</h1>
+                <p>This is about page</p>
+            </div>
+        </div>
+    )
+}
